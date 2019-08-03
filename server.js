73d6b629055bb1fe3fab97b1e1c5447085b1ed9c
Chuntong Gao <heihegao@gmail.com>
2019-08-03 11:16:59 +0200

    implement database connection

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+
+const DATABASE = process.env.DATABASE.replace(
+	'<PASSWORD>',
+	process.env.DATABASE_PASSWORD
+);
+
+mongoose
+	.connect(DATABASE, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false
+	})
+	.then(connection => {
+		console.log(connection.connections);
+		console.log('Database connection successful!');
+	});
 
 const port = process.env.PORT || 3300;
 
