19527c25d24d7fa839031ca52e2cd48cbbcfbf54
Chuntong Gao <heihegao@gmail.com>
2019-08-04 13:35:13 +0200

    implement advanced filtering in getAllTours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -7,10 +7,21 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
 	try {
 		// BUILD QUERY
+		// 1) Filtering
 		const queryObj = { ...req.query };
 		const excludedFields = [ 'page', 'sort', 'limit', 'fields' ];
 		excludedFields.forEach(field => delete queryObj[field]);
-		const query = await Tour.find(queryObj);
+
+		// 2) Advanced filtering
+		let queryStr = JSON.stringify(queryObj);
+		queryStr = queryStr.replace(
+			/\b(gte|gt|lte|lt)\b/g,
+			match => `$${match}`
+		);
+
+		console.log(JSON.parse(queryStr));
+
+		const query = Tour.find(JSON.parse(queryStr));
 
 		// EXECUTE QUERY
 		const tours = await query;
