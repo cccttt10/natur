ab0df00dccc58ca96e4a07fb66e744aba78b07e4
Chuntong Gao <heihegao@gmail.com>
2019-08-04 11:41:48 +0200

    implement deleteTour with database

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -65,11 +65,14 @@ exports.updateTour = async (req, res) => {
 	}
 };
 
-exports.deleteTour = (req, res) => {
-	// const tour = tours.find(tour => tour.id === req.params.id);
-
-	res.status(204).json({
-		status: 'success',
-		data: null
-	});
+exports.deleteTour = async (req, res) => {
+	try {
+		await Tour.findByIdAndDelete(req.params.id);
+		res.status(204).json({
+			status: 'success',
+			data: null
+		});
+	} catch (err) {
+		res.status(404).json({ status: 'fail', message: err });
+	}
 };
