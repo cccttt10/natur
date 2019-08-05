41a9af23f5536dcbca46a0f81babc7c587f1c3a7
Chuntong Gao <heihegao@gmail.com>
2019-08-05 11:52:40 +0200

    implement pagination in getAllTours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -38,6 +38,18 @@ exports.getAllTours = async (req, res) => {
 		}
 		else query = query.select('-__v');
 
+		// 4) Pagination
+		// e.g. page=2&limit=10
+		const page = req.query.page * 1 || 1; // convert string to number
+		const limit = req.query.limit * 1 || 100;
+		const skip = (page - 1) * limit;
+		query = query.skip(skip).limit(limit);
+		if (req.query.page) {
+			const numTours = await Tour.countDocuments();
+			if (skip >= numTours)
+				throw new Error('This page does not exists');
+		}
+
 		// EXECUTE QUERY
 		const tours = await query;
 
