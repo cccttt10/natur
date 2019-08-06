4876573c95c506cd1558b153a1ad419de8549854
Chuntong Gao <heihegao@gmail.com>
2019-08-06 17:31:37 +0200

    respond with 404 when no tour is found with given ID

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
 	req.query.limit = '5';
@@ -29,6 +30,8 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
 	const tour = await Tour.findById(req.params.id);
+	if (!tour)
+		return next(new AppError('No tour found with that ID', 404));
 	res.status(200).json({
 		status: 'success',
 		data: { tour }
