b81845a634773e2630a778646dd0cd80693c262e
Chuntong Gao <heihegao@gmail.com>
2019-08-06 16:29:04 +0200

    delete unnecessary middlewares

--- app.js
+++ app.js
@@ -4,14 +4,11 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const app = express();
 
+// MIDDLEWARES
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-	console.log('Hello from the middleware!');
-	next();
-});
 app.use((req, res, next) => {
 	req.requestTime = new Date().toISOString();
 	next();
