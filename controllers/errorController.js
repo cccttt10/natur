de8bcecd57a3fd4cb257fb34f33a48cfdf14fea6
Chuntong Gao <heihegao@gmail.com>
2019-08-06 18:47:23 +0200

    handle validation errors from database

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -6,10 +6,16 @@ const handleCastErrorDB = err => {
 };
 
 const handleDuplicateFieldsDB = err => {
-	const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];  
+	const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
 	const message = `Duplicate field value: ${value}. Please use another value!`;
 	return new AppError(message, 400);
-  };
+};
+
+const handleValidationErrorDB = err => {
+	const errors = Object.values(err.errors).map(el => el.message);
+	const message = `Invalid input data. ${errors.join('. ')}`;
+	return new AppError(message, 400);
+};
 
 const sendErrorDev = (err, res) => {
 	res.status(err.statusCode).json({
@@ -51,8 +57,10 @@ module.exports = (err, req, res, next) => {
 		let error = { ...err };
 		if (error.name === 'CastError')
 			error = handleCastErrorDB(error);
-		if(error.code === 11000)	
+		if (error.code === 11000)
 			error = handleDuplicateFieldsDB(error);
+		if (error.name === 'ValidationError')
+			error = handleValidationErrorDB(error);
 		sendErrorProd(error, res);
 	}
 };
