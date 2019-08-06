d533c51769507fa95f7cdcc354cb6e45b63ef63a
Chuntong Gao <heihegao@gmail.com>
2019-08-06 10:46:39 +0200

    refactor and move filter, sort, limitFields and paginate into a separate module called APIFeatures

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,5 @@
 const Tour = require('../models/tourModel');
-
-// const tours = JSON.parse(
-// 	fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-// );
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.aliasTopTours = (req, res, next) => {
 	req.query.limit = '5';
@@ -14,52 +11,13 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
 	try {
-		// BUILD QUERY
-		// 1A) Filtering
-		const queryObj = { ...req.query };
-		const excludedFields = [ 'page', 'sort', 'limit', 'fields' ];
-		excludedFields.forEach(field => delete queryObj[field]);
-
-		// 1B) Advanced filtering
-		let queryStr = JSON.stringify(queryObj);
-		queryStr = queryStr.replace(
-			/\b(gte|gt|lte|lt)\b/g,
-			match => `$${match}`
-		);
-
-		console.log(JSON.parse(queryStr));
-
-		let query = Tour.find(JSON.parse(queryStr));
-
-		// 2) Sorting
-		if (req.query.sort) {
-			const sortBy = req.query.sort.split(',').join(' ');
-			console.log(sortBy);
-			query = query.sort(sortBy);
-		}
-		else query = query.sort('-createdAt');
-
-		// 3) Field limiting
-		if (req.query.fields) {
-			const fields = req.query.fields.split(',').join(' ');
-			query = query.select(fields);
-		}
-		else query = query.select('-__v');
-
-		// 4) Pagination
-		// e.g. page=2&limit=10
-		const page = req.query.page * 1 || 1; // convert string to number
-		const limit = req.query.limit * 1 || 100;
-		const skip = (page - 1) * limit;
-		query = query.skip(skip).limit(limit);
-		if (req.query.page) {
-			const numTours = await Tour.countDocuments();
-			if (skip >= numTours)
-				throw new Error('This page does not exists');
-		}
-
 		// EXECUTE QUERY
-		const tours = await query;
+		const features = new APIFeatures(Tour.find(), req.query)
+			.filter()
+			.sort()
+			.limitFields()
+			.paginate();
+		const tours = await features.query;
 
 		// SEND RESPONSE
 		res.status(200).json({
