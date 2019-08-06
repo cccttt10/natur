625f974d48fdec4fb79e8900c4dfca858fb89bb9
Chuntong Gao <heihegao@gmail.com>
2019-08-06 16:33:12 +0200

    handle undefined routes

--- app.js
+++ app.js
@@ -17,5 +17,11 @@ app.use((req, res, next) => {
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.all('*', (req, res, next) => {
+	res.status(404).json({
+		status: 'fail',
+		message: `Can't find ${req.originalUrl} on this server`
+	});
+});
 
 module.exports = app;
