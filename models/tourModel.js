58d23234c75cae6b17ff2bfaf05cca9e6a48c457
Chuntong Gao <heihegao@gmail.com>
2019-08-06 12:30:18 +0200

    hide secret tours using query middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -54,7 +54,11 @@ const tourSchema = new mongoose.Schema(
 			default: Date.now(),
 			select: false // never expose createdAt to client
 		},
-		startDates: [ Date ]
+		startDates: [ Date ],
+		secretTour: {
+			type: Boolean,
+			default: false
+		}
 	},
 	{
 		toJSON: { virtuals: true },
@@ -81,6 +85,21 @@ tourSchema.pre('save', function(next) {
 // 	console.log(doc);
 // 	next();
 // });
+
+// QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function(next) {
+	this.find({ secretTour: { $ne: true } });
+	this.start = Date.now();
+	next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+	console.log(
+		`Query took ${Date.now() - this.start} milliseconds!`
+	);
+	console.log(docs);
+	next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
