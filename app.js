be7420ac8eb137073cd60e36494599d8c03f943a
Chuntong Gao <heihegao@gmail.com>
2019-08-06 16:44:12 +0200

    create error handling middleware

--- app.js
+++ app.js
@@ -18,9 +18,26 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.all('*', (req, res, next) => {
-	res.status(404).json({
-		status: 'fail',
-		message: `Can't find ${req.originalUrl} on this server`
+	// res.status(404).json({
+	// 	status: 'fail',
+	// 	message: `Can't find ${req.originalUrl} on this server`
+	// });
+	const err = new Error(
+		`Can't find ${req.originalUrl} on this server`
+	);
+	err.status = 'fail';
+	err.statusCode = 404;
+	next(err);
+});
+
+// ERROR HANDLING MIDDLEWARE
+app.use((err, req, res, next) => {
+	err.statusCode = err.statusCode || 500;
+	err.status = err.status || 'error';
+
+	res.status(err.statusCode).json({
+		status: err.status,
+		message: err.message
 	});
 });
 
