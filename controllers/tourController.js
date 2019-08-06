2c96bf532a3140cf4fd6deb0cfa71bde1813858c
Chuntong Gao <heihegao@gmail.com>
2019-08-06 17:35:27 +0200

    respond with 404 when attempting to update / delete a tour that does not exist

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -30,8 +30,10 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
 	const tour = await Tour.findById(req.params.id);
+
 	if (!tour)
 		return next(new AppError('No tour found with that ID', 404));
+
 	res.status(200).json({
 		status: 'success',
 		data: { tour }
@@ -54,6 +56,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 			runValidators: true
 		}
 	);
+
+	if (!tour)
+		return next(new AppError('No tour found with that ID', 404));
+
 	res.status(200).json({
 		status: 'success',
 		data: {
@@ -63,7 +69,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-	await Tour.findByIdAndDelete(req.params.id);
+	const tour = await Tour.findByIdAndDelete(req.params.id);
+
+	if (!tour)
+		return next(new AppError('No tour found with that ID', 404));
+
 	res.status(204).json({
 		status: 'success',
 		data: null
