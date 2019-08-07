66a191e6ca943715c4e135588c1ba60d27211dab
Chuntong Gao <heihegao@gmail.com>
2019-08-07 10:56:58 +0200

    send token to user after signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
@@ -7,10 +8,17 @@ exports.signup = catchAsync(async (req, res, next) => {
 		email: req.body.email,
 		password: req.body.password,
 		passwordConfirm: req.body.passwordConfirm
-	})
-    
+	});
+
+	const token = jwt.sign(
+		{ id: newUser._id },
+		process.env.JWT_SECRET,
+		{ expiresIn: process.env.JWT_EXPIRES_IN }
+	);
+
 	res.status(201).json({
 		status: 'success',
+		token,
 		data: {
 			user: newUser
 		}
