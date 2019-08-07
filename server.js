0dfc96a7db9468800d435b765291a1750ecfcf8c
Chuntong Gao <heihegao@gmail.com>
2019-08-07 08:54:18 +0200

    handle uncaught exceptiojns

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+	console.log('UNCAUGHT EXCEPTION 🥵 Shutting down...');
+	console.log(err.name, err.message);
+	process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -27,7 +33,8 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', err => {
-	console.log(err.name, err.message);
 	console.log('UNHANDLED REJECTION 🥵 Shutting down...');
+	console.log(err.name, err.message);
 	server.close(() => process.exit(1));
 });
+
