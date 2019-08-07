20b5db811f3f3b86f408bce5963e14dbec2d819b
Chuntong Gao <heihegao@gmail.com>
2019-08-07 11:15:14 +0200

    Do not send password to client

--- models/userModel.js
+++ models/userModel.js
@@ -21,7 +21,8 @@ const userSchema = new mongoose.Schema({
 	password: {
 		type: String,
 		required: [ true, 'Please provide a password' ],
-		minlength: 8
+		minlength: 8,
+		select: false
 	},
 	passwordConfirm: {
 		type: String,
