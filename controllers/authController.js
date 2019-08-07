0b3ce24b91e0fb50dd30445d7effa67555abfa14
Chuntong Gao <heihegao@gmail.com>
2019-08-07 11:05:27 +0200

    very basic implementation of login function in authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
 	const newUser = await User.create({
@@ -24,3 +25,20 @@ exports.signup = catchAsync(async (req, res, next) => {
 		}
 	});
 });
+
+exports.login = (req, res, next) => {
+	const { email, password } = req.body;
+
+	// 1) Check if email and password exist
+	if (!email || !password)
+		next(new AppError('Please provide email and password!'), 400);
+
+	// 2) Check if user exists && password is correct
+
+	// 3) If everything ok, send token to client
+	const token = 'fake toekn';
+	res.status(200).json({
+		status: 'success',
+		token
+	});
+};
