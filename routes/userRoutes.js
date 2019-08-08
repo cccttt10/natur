742da210f05d87e5d1fb0478e97d5efa97598362
Chuntong Gao <heihegao@gmail.com>
2019-08-08 09:02:01 +0200

    add deleteMe route to allow user to delete himself (actually deactivate account)

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -22,6 +22,12 @@ router.patch(
 	userController.updateMe
 );
 
+router.delete(
+	'/deleteMe',
+	authController.protect,
+	userController.deleteMe
+);
+
 router
 	.route('/')
 	.get(userController.getAllUsers)
