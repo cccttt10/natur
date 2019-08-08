49dc404f2a6403661a1f1c22204a0f9e524a4bfa
Chuntong Gao <heihegao@gmail.com>
2019-08-08 19:58:40 +0200

    import data from tours.json instead of tours-simple.json, to include geospatial data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -23,7 +23,7 @@ mongoose
 
 // READ JSON FILE
 const tours = JSON.parse(
-	fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+	fs.readFileSync(`${__dirname}/tours.json`, 'utf-8')
 );
 
 // IMPORT DATA INTO DATABASE
