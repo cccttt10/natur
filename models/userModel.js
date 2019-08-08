aaa72a2791ae66e34baedec1b11cd1a65f379838
Chuntong Gao <heihegao@gmail.com>
2019-08-08 09:01:31 +0200

    add 'active' field to userSchema and return only active users upon find queries

--- models/userModel.js
+++ models/userModel.js
@@ -45,7 +45,12 @@ const userSchema = new mongoose.Schema({
 	},
 	passwordChangedAt: Date,
 	passwordResetToken: String,
-	passwordResetExpires: Date
+	passwordResetExpires: Date,
+	active: {
+		type: Boolean,
+		default: true,
+		select: false
+	}
 });
 
 userSchema.pre('save', async function(next) {
@@ -63,6 +68,12 @@ userSchema.pre('save', async function(next) {
 userSchema.pre('save', function(next) {
 	if (!this.isModified('password') || this.isNew) return next();
 	this.passwordChangedAt = Date.now() - 1000;
+	next();
+});
+
+userSchema.pre(/^find/, function(next) {
+	// 'this' points to current query
+	this.find({ active: { $ne: false } });
 	next();
 });
 
