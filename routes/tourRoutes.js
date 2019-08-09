2ef3244d1067df7a2cb92ee0b75cf60ff843e1e5
Chuntong Gao <heihegao@gmail.com>
2019-08-09 17:24:41 +0200

    implement nested routes to allow user to create a review on a tour, specifying the tour in the request url instead of in the request body

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 
 const router = express.Router();
 
@@ -29,6 +30,18 @@ router
 		authController.protect,
 		authController.restrictTo('admin', 'lead-guide'),
 		tourController.deleteTour
+	);
+
+// POST /tour/1234/reviews
+// GET  /tour/1234/reviews
+// GET  /tour/1234/reviews/5678
+
+router
+	.route('/:tourId/reviews')
+	.post(
+		authController.protect,
+		authController.restrictTo('user'),
+		reviewController.createReview
 	);
 
 module.exports = router;
