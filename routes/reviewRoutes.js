d705f760cc2b413b56e70bdbdd6e10a2742adc01
Chuntong Gao <heihegao@gmail.com>
2019-08-09 16:12:46 +0200

    basic import & setup in reviewRoutes.js

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const reviewController = require('../controllers/reviewController');
+
+const router = express.Router();
+
+router.route('/');
+
+module.exports = router;
