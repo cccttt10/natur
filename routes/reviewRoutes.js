534c5860fc202be06d9ba17ae8acb019b3913a32
Chuntong Gao <heihegao@gmail.com>
2019-08-09 16:43:43 +0200

    implement get and post routes in reviewRoutes.js

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const reviewController = require('../controllers/reviewController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.route('/');
+router
+	.route('/')
+	.get(reviewController.getAllReviews)
+	.post(
+		authController.protect,
+		authController.restrictTo('user'),
+		reviewController.createReview
+	);
 
 module.exports = router;
