16de249373067d2c21b986057a3f72d4690a3a9c
Chuntong Gao <heihegao@gmail.com>
2019-08-09 16:15:04 +0200

    import and mount review route in app.js

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
@@ -23,6 +26,8 @@ app.use((req, res, next) => {
 // 2) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
+
 app.all('*', (req, res, next) => {
 	next(
 		new AppError(
