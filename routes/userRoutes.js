85d1592a401598838c7f2562690996f3743ee21e
Chuntong Gao <heihegao@gmail.com>
2019-08-10 21:51:22 +0200

    restrict some routes in userRoutes.js to admin only, using middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,20 +9,21 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
+// Protect all routes after this middleware
 router.use(authController.protect);
 
 router.patch('/updateMyPassword', authController.updatePassword);
-
 router.get('/me', userController.getMe, userController.getUser);
-
 router.patch('/updateMe', userController.updateMe);
-
 router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
 
 router
 	.route('/')
 	.get(userController.getAllUsers)
 	.post(userController.createUser);
+	
 router
 	.route('/:id')
 	.get(userController.getUser)
