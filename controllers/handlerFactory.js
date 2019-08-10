6813ebb53cbf94d99fb119009e1273dd5b728456
Chuntong Gao <heihegao@gmail.com>
2019-08-10 17:20:13 +0200

    add filter object to getAll function to allow for nested GET reviews on tour

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -68,8 +68,13 @@ exports.getOne = (Model, populateOptions) =>
 
 exports.getAll = Model =>
 	catchAsync(async (req, res, next) => {
+
+		// To allow for nested GET reviews on tour
+		let filter = {};
+		if (req.params.tourId) filter = { tour: req.params.tourId };
+
 		// EXECUTE QUERY
-		const features = new APIFeatures(Model.find(), req.query)
+		const features = new APIFeatures(Model.find(filter), req.query)
 			.filter()
 			.sort()
 			.limitFields()
