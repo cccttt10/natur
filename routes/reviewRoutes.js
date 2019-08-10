d84a222f03496ec31d16f6fc6cd2f2f849b65ce0
Chuntong Gao <heihegao@gmail.com>
2019-08-10 10:41:24 +0200

    implement nested route for getting all reviews on a tour

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -5,6 +5,7 @@ const authController = require('../controllers/authController');
 const router = express.Router({ mergeParams: true });
 
 // POST /tours/1234/reviews
+// GET  /tours/1234/reviews
 // POST /reviews
 
 router
