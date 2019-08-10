efda8954409e267ea2e93daecf5ce0753e6c81c1
Chuntong Gao <heihegao@gmail.com>
2019-08-10 17:31:09 +0200

    implement /me route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,6 +10,13 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
+router.get(
+	'/me',
+	authController.protect,
+	userController.getMe,
+	userController.getUser
+);
+
 router.patch(
 	'/updateMyPassword',
 	authController.protect,
