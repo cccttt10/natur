55c75684468d0fb2e543ecc09dd81ca900086e8b
Chuntong Gao <heihegao@gmail.com>
2019-08-10 17:04:00 +0200

    use getOne function when getting a user by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -64,16 +64,13 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
 	});
 });
 
-exports.getUser = (req, res) => {
-	res.status(500).json({
-		status: 'error',
-		message: 'This route is not yet defined'
-	});
-};
+exports.getUser = factory.getOne(User);
+
 exports.createUser = (req, res) => {
 	res.status(500).json({
 		status: 'error',
-		message: 'This route is not yet defined'
+		message:
+			'This route is not yet defined. Please use /singup route instead.'
 	});
 };
 
