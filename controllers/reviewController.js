dc64911e783e9cc68c8cc70e51a9b96664c9db2c
Chuntong Gao <heihegao@gmail.com>
2019-08-10 17:20:44 +0200

    use getAll function when getting all reviews / getting all users

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,19 +1,5 @@
 const Review = require('../models/reviewModel');
-const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
-
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-	let filter = {};
-	if (req.params.tourId) filter = { tour: req.params.tourId };
-
-	const reviews = await Review.find(filter);
-
-	res.status(200).json({
-		status: 'success',
-		results: reviews.length,
-		data: { reviews }
-	});
-});
 
 exports.setTourUserIds = (req, res, next) => {
 	// Allow nested routes
@@ -22,6 +8,7 @@ exports.setTourUserIds = (req, res, next) => {
 	next();
 };
 
+exports.getAllReviews = factory.getAll(Review);
 exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);
--- controllers/userController.js
+++ controllers/userController.js
@@ -12,17 +12,6 @@ const filterObj = (obj, ...allowedFields) => {
 	return filteredObj;
 };
 
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-	const users = await User.find();
-
-	// SEND RESPONSE
-	res.status(200).json({
-		status: 'success',
-		results: users.length,
-		data: { users }
-	});
-});
-
 exports.updateMe = catchAsync(async (req, res, next) => {
 	// 1) Create error if user tries to update password
 	//    and posts password data
@@ -64,8 +53,6 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
 	});
 });
 
-exports.getUser = factory.getOne(User);
-
 exports.createUser = (req, res) => {
 	res.status(500).json({
 		status: 'error',
@@ -76,5 +63,6 @@ exports.createUser = (req, res) => {
 
 // Do NOT update passwords with this!
 exports.updateUser = factory.updateOne(User);
-
 exports.deleteUser = factory.deleteOne(User);
+exports.getAllUsers = factory.getAll(User);
+exports.getUser = factory.getOne(User);
