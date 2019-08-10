4247dcaf63eade6be3af7ea896d93c2cd90ddefe
Chuntong Gao <heihegao@gmail.com>
2019-08-10 16:47:54 +0200

    implement createOne function using factory pattern

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -39,3 +39,11 @@ exports.updateOne = Model =>
 			}
 		});
 	});
+
+exports.createOne = Model =>
+	catchAsync(async (req, res, next) => {
+		const doc = await Model.create(req.body);
+		res
+			.status(201)
+			.json({ status: 'success', data: { data: doc } });
+	});
