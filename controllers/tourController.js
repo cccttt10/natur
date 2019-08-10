2716f9adb6c1133270c51fe026225988a536b4a9
Chuntong Gao <heihegao@gmail.com>
2019-08-10 16:56:48 +0200

    use getOne function when getting a tour by id

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -29,34 +29,10 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 	});
 });
 
-exports.getTour = catchAsync(async (req, res, next) => {
-	const tour = await Tour.findById(req.params.id).populate(
-		'reviews'
-	);
-
-	if (!tour)
-		return next(new AppError('No tour found with that ID', 404));
-
-	res.status(200).json({
-		status: 'success',
-		data: { tour }
-	});
-});
-
+exports.getTour = factory.getOne(Tour, { path: 'reviews' });
 exports.createTour = factory.createOne(Tour);
 exports.updateTour = factory.updateOne(Tour);
 exports.deleteTour = factory.deleteOne(Tour);
-// exports.deleteTour = catchAsync(async (req, res, next) => {
-// 	const tour = await Tour.findByIdAndDelete(req.params.id);
-
-// 	if (!tour)
-// 		return next(new AppError('No tour found with that ID', 404));
-
-// 	res.status(204).json({
-// 		status: 'success',
-// 		data: null
-// 	});
-// });
 
 exports.getTourStats = catchAsync(async (req, res, next) => {
 	const stats = await Tour.aggregate([
