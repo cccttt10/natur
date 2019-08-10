31b39c4af7c4eefb1754cc0f7bb8843b9950fee0
Chuntong Gao <heihegao@gmail.com>
2019-08-10 16:54:18 +0200

    implement getOne function using factory pattern

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -47,3 +47,20 @@ exports.createOne = Model =>
 			.status(201)
 			.json({ status: 'success', data: { data: doc } });
 	});
+
+exports.getOne = (Model, populateOptions) =>
+	catchAsync(async (req, res, next) => {
+		let query = Model.findById(req.params.id);
+		if (populateOptions) query = query.populate(populateOptions);
+		const doc = await query;
+
+		if (!doc)
+			return next(
+				new AppError('No document found with that ID', 404)
+			);
+
+		res.status(200).json({
+			status: 'success',
+			data: { data: doc }
+		});
+	});
