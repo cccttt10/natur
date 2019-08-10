8f32c487997dcf8e9cc793282ffcc7ca404a9922
Chuntong Gao <heihegao@gmail.com>
2019-08-10 17:39:56 +0200

    expose getAllTours functionality to everyone, i.e. make it unprotected

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -24,7 +24,7 @@ router
 
 router
 	.route('/')
-	.get(authController.protect, tourController.getAllTours)
+	.get(tourController.getAllTours)
 	.post(tourController.createTour);
 router
 	.route('/:id')
