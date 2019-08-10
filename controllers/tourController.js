334a65d9894dc3d9a49af8afd79e9376118d94ae
Chuntong Gao <heihegao@gmail.com>
2019-08-10 16:49:48 +0200

    use createOne function when creating a tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -43,15 +43,8 @@ exports.getTour = catchAsync(async (req, res, next) => {
 	});
 });
 
-exports.createTour = catchAsync(async (req, res, next) => {
-	const newTour = await Tour.create(req.body);
-	res
-		.status(201)
-		.json({ status: 'success', data: { tour: newTour } });
-});
-
+exports.createTour = factory.createOne(Tour);
 exports.updateTour = factory.updateOne(Tour);
-
 exports.deleteTour = factory.deleteOne(Tour);
 // exports.deleteTour = catchAsync(async (req, res, next) => {
 // 	const tour = await Tour.findByIdAndDelete(req.params.id);
