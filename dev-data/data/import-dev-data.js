16c8cf93b7a20719846175c53dade26042e2689f
Chuntong Gao <heihegao@gmail.com>
2019-08-10 22:17:42 +0200

    change import script to import complete data about users, tours and reviews

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -25,27 +27,37 @@ mongoose
 const tours = JSON.parse(
 	fs.readFileSync(`${__dirname}/tours.json`, 'utf-8')
 );
+const users = JSON.parse(
+	fs.readFileSync(`${__dirname}/users.json`, 'utf-8')
+);
+const reviews = JSON.parse(
+	fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
 // IMPORT DATA INTO DATABASE
 const importData = async () => {
 	try {
 		await Tour.create(tours);
+		await User.create(users, { validateBeforeSave: false });
+		await Review.create(reviews);
 		console.log('Data successfully loaded!');
 	} catch (err) {
-        console.log(err);
+		console.log(err);
 	}
-    process.exit();
+	process.exit();
 };
 
 // DELETE ALL EXISTING DATA FROM DATABASE
 const deleteData = async () => {
 	try {
 		await Tour.deleteMany();
+		await User.deleteMany();
+		await Review.deleteMany();
 		console.log('Existing data successfully deleted!');
 	} catch (err) {
-        console.log(err);
+		console.log(err);
 	}
-    process.exit();
+	process.exit();
 };
 
 if (process.argv[2] === '--import') importData();
