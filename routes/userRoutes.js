9e86ca3e3d287ee19965c57d28cef94c84aadc3f
Chuntong Gao <heihegao@gmail.com>
2019-08-10 21:46:44 +0200

    use authController.protect middleware for protected routes in userRoutes.js, also reduce code duplication

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,34 +6,18 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.get(
-	'/me',
-	authController.protect,
-	userController.getMe,
-	userController.getUser
-);
+router.use(authController.protect);
 
-router.patch(
-	'/updateMyPassword',
-	authController.protect,
-	authController.updatePassword
-);
+router.patch('/updateMyPassword', authController.updatePassword);
 
-router.patch(
-	'/updateMe',
-	authController.protect,
-	userController.updateMe
-);
+router.get('/me', userController.getMe, userController.getUser);
 
-router.delete(
-	'/deleteMe',
-	authController.protect,
-	userController.deleteMe
-);
+router.patch('/updateMe', userController.updateMe);
+
+router.delete('/deleteMe', userController.deleteMe);
 
 router
 	.route('/')
