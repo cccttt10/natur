860f92a31eedcb7fca3e372b873a7394e4263b62
Chuntong Gao <heihegao@gmail.com>
2019-08-10 17:30:56 +0200

    implement getMe function in userController.js to allow the currently logged in user to retrieve info about himself

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,6 +12,11 @@ const filterObj = (obj, ...allowedFields) => {
 	return filteredObj;
 };
 
+exports.getMe = (req, res, next) => {
+	req.params.id = req.user.id;
+	next();
+};
+
 exports.updateMe = catchAsync(async (req, res, next) => {
 	// 1) Create error if user tries to update password
 	//    and posts password data
