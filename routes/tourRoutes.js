14a5b2d96fa966f89c34f0d7c4df37b722805d36
Chuntong Gao <heihegao@gmail.com>
2019-08-10 17:44:18 +0200

    allow only admin, lead guides and guides to get monthly plans

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -20,7 +20,11 @@ router.route('/tour-stats').get(tourController.getTourStats);
 
 router
 	.route('/monthly-plan/:year')
-	.get(tourController.getMonthlyPlan);
+	.get(
+		authController.protect,
+		authController.restrictTo('admin', 'lead-guide', 'guide'),
+		tourController.getMonthlyPlan
+	);
 
 router
 	.route('/')
