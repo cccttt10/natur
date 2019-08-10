29e7e53f1b1a59aad0f7246ab36fc68d5bd52268
Chuntong Gao <heihegao@gmail.com>
2019-08-10 11:16:57 +0200

    user updateOne when updating a user

--- controllers/userController.js
+++ controllers/userController.js
@@ -76,11 +76,7 @@ exports.createUser = (req, res) => {
 		message: 'This route is not yet defined'
 	});
 };
-exports.updateUser = (req, res) => {
-	res.status(500).json({
-		status: 'error',
-		message: 'This route is not yet defined'
-	});
-};
+
+exports.updateUser = factory.updateOne(User);
 
 exports.deleteUser = factory.deleteOne(User);
