f6eb5b0e6e7393654034031f611fa42a70537ec3
Chuntong Gao <heihegao@gmail.com>
2019-08-11 18:30:17 +0200

    calculate average rating upon creating a new review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
 	{
@@ -45,6 +46,31 @@ reviewSchema.pre(/^find/, function(next) {
 		select: 'name photo'
 	});
 	next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function(tourId) {
+	const stats = await this.aggregate([
+		{
+			$match: { tour: tourId }
+		},
+		{
+			$group: {
+				_id: '$tour',
+				nRating: { $sum: 1 },
+				avgRating: { $avg: '$rating' }
+			}
+		}
+	]);  
+
+	await Tour.findByIdAndUpdate(tourId, {
+		ratingsQuantity: stats[0].nRating,
+		ratingsAverage: stats[0].avgRating
+	});
+};
+
+reviewSchema.post('save', function() {
+	// 'this' points to current review document
+	this.constructor.calcAverageRatings(this.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
