b0afb77088cad90af20a7c1e26720441e7d62211
Chuntong Gao <heihegao@gmail.com>
2019-08-13 17:14:14 +0200

    read token from req.cookies.jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,7 +34,7 @@ const createSendToken = (user, statusCode, res) => {
 
 	// Remove password from response
 	user.password = undefined;
-	
+
 	res.status(statusCode).json({
 		status: 'success',
 		token,
@@ -84,6 +84,8 @@ exports.protect = catchAsync(async (req, res, next) => {
 		req.headers.authorization.startsWith('Bearer')
 	)
 		token = req.headers.authorization.split(' ')[1];
+	else if (req.cookies.jwt) token = req.cookies.jwt;
+	
 	if (!token)
 		return next(
 			new AppError(
