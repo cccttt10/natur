310b88ddbe7eb1c280d81e7f8a1f2560b73afc2e
Chuntong Gao <heihegao@gmail.com>
2019-08-13 08:42:17 +0200

    display cards with static data in overview page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,18 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+	// 1) Get tour data from collection
+    const tours = await Tour.find();
+    
+	// 2) Build template
+
+	// 3) Render that template using tour data from 1)
 	res.status(200).render('overview', {
-		title: 'All Tours'
+        title: 'All Tours',
+        tours
 	});
-};
+});
 
 exports.getTour = (req, res) => {
 	res.status(200).render('tour', {
