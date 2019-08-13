15c6b14fe82c9f0a53db6edf6fd1e714c19d04b8
Chuntong Gao <heihegao@gmail.com>
2019-08-13 17:36:03 +0200

    user isLoggedIn middleware for every view route

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,12 +4,9 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.use(authController.isLoggedIn);
 router.get('/', viewController.getOverview);
-router.get(
-	'/tour/:slug',
-	authController.protect,
-	viewController.getTour
-);
+router.get('/tour/:slug', viewController.getTour);
 router.get('/login', viewController.getLoginForm);
 
 module.exports = router;
