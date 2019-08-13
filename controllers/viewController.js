71f699d26011451bde11cafb0350d0f99eec3023
Chuntong Gao <heihegao@gmail.com>
2019-08-13 10:24:17 +0200

    implement getTour function in viewController.js

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -3,19 +3,31 @@ const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
 	// 1) Get tour data from collection
-    const tours = await Tour.find();
-    
+	const tours = await Tour.find();
+
 	// 2) Build template
 
 	// 3) Render that template using tour data from 1)
 	res.status(200).render('overview', {
-        title: 'All Tours',
-        tours
+		title: 'All Tours',
+		tours
 	});
 });
 
-exports.getTour = (req, res) => {
-	res.status(200).render('tour', {
-		title: 'The Forest Hiker Tour'
+exports.getTour = catchAsync(async (req, res, next) => {
+	// 1) Get data for requested tour (including reviews and guides)
+	const tour = await Tour.findOne({
+		slug: req.params.slug
+	}).populate({
+		path: 'reviews',
+		fields: 'review rating user'
 	});
-};
+
+	// 2) Build template
+
+	// 3) Render template using data from 1)
+	res.status(200).render('tour', {
+		title: 'The Forest Hiker Tour',
+		tour
+	});
+});
