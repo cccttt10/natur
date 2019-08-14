5e993f417d3b7ba781dd81a90180490c4d680712
Chuntong Gao <heihegao@gmail.com>
2019-08-14 16:50:34 +0200

    add event listener for log out button

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,11 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
-import { login } from './login';
+import { login, logout } from './login';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form');
+const logoutBtn = document.querySelector('.nav__el--logout');
 
 // VALUES
 
@@ -21,3 +22,5 @@ if (loginForm)
 		const password = document.getElementById('password').value;
 		login(email, password);
 	});
+
+if (logoutBtn) logoutBtn.addEventListener('click', logout);
