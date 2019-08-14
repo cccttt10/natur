1040597c69ae48ac2a6060a054082836350befa4
Chuntong Gao <heihegao@gmail.com>
2019-08-14 22:22:25 +0200

    if tour not found for requested tour page, produce error message

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
 	// 1) Get tour data from collection
@@ -22,6 +23,11 @@ exports.getTour = catchAsync(async (req, res, next) => {
 		path: 'reviews',
 		fields: 'review rating user'
 	});
+
+	if (!tour)
+		return next(
+			new AppError('There is no tour with that name', 404)
+		);
 
 	// 2) Build template
 
