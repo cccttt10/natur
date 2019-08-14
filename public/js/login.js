300b78d088e2ee08f811da9414250dcf1e36a183
Chuntong Gao <heihegao@gmail.com>
2019-08-14 14:42:27 +0200

    complete login functionality on front end

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,7 @@
-const login = async (email, password) => {
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+export const login = async (email, password) => {
 	try {
 		const res = await axios({
 			method: 'POST',
@@ -7,16 +10,10 @@ const login = async (email, password) => {
 		});
 
 		if (res.data.status === 'success')
-			alert('Logged in successfully');
-		window.setTimeout(() => location.assign('/'), 1500);
+			showAlert('success ', 'Logged in successfully');
+
+		window.setTimeout(() => location.assign('/'), 800);
 	} catch (err) {
-		alert(err.response.data.message);
+		showAlert('error', err.response.data.message);
 	}
 };
-
-document.querySelector('.form').addEventListener('submit', e => {
-	e.preventDefault();
-	const email = document.getElementById('email').value;
-	const password = document.getElementById('password').value;
-	login(email, password);
-});
