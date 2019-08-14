1ec54204fad0a6b7e2f42c901b5f34f8f8d07edf
Chuntong Gao <heihegao@gmail.com>
2019-08-14 16:50:01 +0200

    implement logout function in authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -76,6 +76,14 @@ exports.login = catchAsync(async (req, res, next) => {
 	createSendToken(user, 200, res);
 });
 
+exports.logout = (req, res) => {
+	res.cookie('jwt', 'loggedout', {
+		expires: new Date(Date.now() + 10 * 1000),
+		httpOnly: true
+	});
+	res.status(200).json({ status: 'success' });
+};
+
 exports.protect = catchAsync(async (req, res, next) => {
 	// 1) Get token and check if it's there
 	let token;
