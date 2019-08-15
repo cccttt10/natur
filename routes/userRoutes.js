4c0757c8c7816e4afbd615587270723e40d650c9
Chuntong Gao <heihegao@gmail.com>
2019-08-15 19:49:10 +0200

    resize non square images using resizeUserPhoto middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -18,6 +18,7 @@ router.get('/me', userController.getMe, userController.getUser);
 router.patch(
 	'/updateMe',
 	userController.uploadUserPhoto,
+	userController.resizeUserPhoto,
 	userController.updateMe
 );
 router.delete('/deleteMe', userController.deleteMe);
