58d53770f297a95f4cd1e416d32ff894884ef5a0
Chuntong Gao <heihegao@gmail.com>
2019-08-15 08:27:09 +0200

    add '/me' route for account page and make it protected

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,9 +4,24 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.use(authController.isLoggedIn);
-router.get('/', viewController.getOverview);
-router.get('/tour/:slug', viewController.getTour);
-router.get('/login', viewController.getLoginForm);
+router.get(
+	'/',
+	authController.isLoggedIn,
+	viewController.getOverview
+);
+
+router.get(
+	'/tour/:slug',
+	authController.isLoggedIn,
+	viewController.getTour
+);
+
+router.get(
+	'/login',
+	authController.isLoggedIn,
+	viewController.getLoginForm
+);
+
+router.get('/me', authController.protect, viewController.getAccount);
 
 module.exports = router;
