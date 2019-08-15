2f20d774226a402f179e8db7fa16a3438c1a2409
Chuntong Gao <heihegao@gmail.com>
2019-08-15 20:59:51 +0200

    create and implement Email class

--- utils/email.js
+++ utils/email.js
@@ -1,26 +1,56 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options => {
-	// 1) Create a transporter
-	const transporter = nodemailer.createTransport({
-		host: process.env.EMAIL_HOST,
-		port: process.env.EMAIL_PORT,
-		auth: {
-			user: process.env.EMAIL_USERNAME,
-			pass: process.env.EMAIL_PASSWORD
+module.exports = class Email {
+	constructor(user, url) {
+		this.to = user.email;
+		this.firstName = user.name.split(' ')[0];
+		this.url = url;
+		this.from = `Chuntong Gao <${process.env.EMAIL_FROM}>`;
+	}
+
+	newTransport() {
+		if (process.env.NODE_ENV === 'production') {
+			// Sendgrid
+			return 1;
 		}
-	});
+		return nodemailer.createTransport({
+			host: process.env.EMAIL_HOST,
+			port: process.env.EMAIL_PORT,
+			auth: {
+				user: process.env.EMAIL_USERNAME,
+				pass: process.env.EMAIL_PASSWORD
+			}
+		});
+	}
 
-	// 2) Define email options
-	const mailOptions = {
-		from: 'Chuntong Gao <heihegao@gmail.com>',
-		to: options.email,
-		subject: options.subject,
-		text: options.message
-	};
+	// Send actual email
+	async send(template, subject) {
+		// 1) Render HTML based on pug template
+		const html = pug.renderFile(
+			`${__dirname}/../views/emails/${template}.pug`,
+			{
+				firstName: this.firstName,
+				url: this.url,
+				subject
+			}
+		);
 
-	// 3) Actually send email
-	await transporter.sendMail(mailOptions);
+		// 2) Define email options
+		const mailOptions = {
+			from: this.from,
+			to: this.to,
+			subject,
+			html,
+			text: htmlToText.fromString(html)
+		};
+
+		// 3) Create transport and send email
+		await this.newTransport().sendMail(mailOptions);
+	}
+
+	async sendWelcome() {
+		await this.send('welcome', 'Welcome to the Natur family!');
+	}
 };
-
-module.exports = sendEmail;
