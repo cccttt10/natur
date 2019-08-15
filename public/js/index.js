76052c5cc9bf84dbf8cfa8ef0712fecc8f8b0e39
Chuntong Gao <heihegao@gmail.com>
2019-08-15 20:00:48 +0200

    implement image uploading on front end

--- public/js/index.js
+++ public/js/index.js
@@ -33,9 +33,19 @@ if (logoutBtn) logoutBtn.addEventListener('click', logout);
 if (userDataForm)
 	userDataForm.addEventListener('submit', e => {
 		e.preventDefault();
-		const name = document.getElementById('name').value;
-		const email = document.getElementById('email').value;
-		updateSettings({ name, email }, 'data');
+
+		// const name = document.getElementById('name').value;
+		// const email = document.getElementById('email').value;
+		// updateSettings({ name, email }, 'data');
+
+		const form = new FormData();
+		form.append('name', document.getElementById('name').value);
+		form.append('email', document.getElementById('email').value);
+		form.append(
+			'photo',
+			document.getElementById('photo').files[0]
+		);
+		updateSettings(form, 'data');
 	});
 
 if (userPasswordForm)
