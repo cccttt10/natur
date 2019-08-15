8ad8c569e20a291a48a2648ca241b69f41389af2
Chuntong Gao <heihegao@gmail.com>
2019-08-15 14:59:51 +0200

    finish update password functionality on front end

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,9 @@ const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector(
+	'.form-user-password'
+);
 
 // VALUES
 
@@ -33,4 +36,32 @@ if (userDataForm)
 		const name = document.getElementById('name').value;
 		const email = document.getElementById('email').value;
 		updateSettings({ name, email }, 'data');
+	});
+
+if (userPasswordForm)
+	userPasswordForm.addEventListener('submit', async e => {
+		e.preventDefault();
+
+		document.querySelector('.btn--save-password').textContent =
+			'Updating...';
+
+		const passwordCurrent = document.getElementById(
+			'password-current'
+		).value;
+		const password = document.getElementById('password').value;
+		const passwordConfirm = document.getElementById(
+			'password-confirm'
+		).value;
+
+		await updateSettings(
+			{ passwordCurrent, password, passwordConfirm },
+			'password'
+		);
+
+		document.querySelector('.btn--save-password').textContent =
+			'Save password';
+
+		document.getElementById('password-current').value = '';
+		document.getElementById('password').value = '';
+		document.getElementById('password-confirm').value = '';
 	});
