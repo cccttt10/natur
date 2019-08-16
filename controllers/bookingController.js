a49074a9329eaceb8137c9464d7cf4e956c3aa31
Chuntong Gao <heihegao@gmail.com>
2019-08-16 14:53:53 +0200

    implement booking routes and corresponding route handlers for CRUD operations

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -48,3 +48,9 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 	await Booking.create({ tour, user, price });
 	res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,10 +4,24 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.use(authController.protect);
+
 router.get(
 	'/checkout-session/:tourId',
-	authController.protect,
 	bookingController.getCheckoutSession
 );
+
+router.use(authController.restrictTo('admin', 'lead-guide'));
+
+router
+	.route('/')
+	.get(bookingController.getAllBookings)
+	.post(bookingController.createBooking);
+
+router
+	.route('/:id')
+	.get(bookingController.getBooking)
+	.patch(bookingController.updateBooking)
+	.delete(bookingController.deleteBooking);
 
 module.exports = router;
