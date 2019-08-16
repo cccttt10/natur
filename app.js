ebe5335b068b6b37c8487ba1860bcf4e8cea8e48
Chuntong Gao <heihegao@gmail.com>
2019-08-16 19:04:23 +0200

    use compression in app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -32,6 +34,8 @@ app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
 // Cookie parser
 app.use(cookieParser());
+
+app.user(compression());
 
 // Test middleware
 app.use((req, res, next) => {
