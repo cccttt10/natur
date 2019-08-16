6273b209713e205c8ff48f44e2663f7d52a5dfea
Chuntong Gao <heihegao@gmail.com>
2019-08-16 08:21:59 +0200

    implement password reset emails

--- controllers/authController.js
+++ controllers/authController.js
@@ -202,19 +202,17 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 	await user.save({ validateBeforeSave: false });
 
 	// 3) Send token to user's email
-	const resetURL = `${req.protocol}://
-	 ${req.get('host')}
-	 /api/v1/users/resetPassword/
-	 ${resetToken}}`;
-
-	const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}. \nIf you didn't forget your password, please ignore this email!`;
-
 	try {
-		// await sendEmail({
-		// 	email: user.email,
-		// 	subject: 'Your password reset token (valid for 10 min)',
-		// 	message
-		// });
+		const resetURL = `${req.protocol}://
+		 ${req.get('host')}
+		 /api/v1/users/resetPassword/${resetToken}}`;
+
+		await new Email(user, resetURL).sendPasswordReset();
+
+		res.status(200).json({
+			status: 'success',
+			message: 'Token sent to email!'
+		});
 	} catch (err) {
 		user.passwordResetToken = undefined;
 		user.passwordResetExpires = undefined;
@@ -226,11 +224,6 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 			)
 		);
 	}
-
-	res.status(200).json({
-		status: 'success',
-		message: 'Token sent to email!'
-	});
 });
 
 exports.resetPassword = catchAsync(async (req, res, next) => {
--- utils/email.js
+++ utils/email.js
@@ -53,4 +53,11 @@ module.exports = class Email {
 	async sendWelcome() {
 		await this.send('welcome', 'Welcome to the Natur family!');
 	}
+
+	async sendPasswordReset() {
+		await this.send(
+			'passwordReset',
+			'Your password reset token (valid for only 10 minutes'
+		);
+	}
 };
